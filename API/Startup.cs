1b2e5d0cb5a68d5c8158e1fe469cef0a89bc6860
steviep <ig2bagd@yahoo.com>
2021-09-25 12:07:11 -0700

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,21 +28,30 @@ namespace API
 
       services.AddApplicationServices();
       services.AddSwaggerDocumentation();
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       app.UseMiddleware<ExceptionMiddleware>();
-      
+
       app.UseSwaggerDocumentation();
 
       app.UseStatusCodePagesWithReExecute("/errors/{0}");
-      
+
       app.UseHttpsRedirection();
 
       app.UseRouting();
       app.UseStaticFiles();
+
+      app.UseCors("CorsPolicy");
 
       app.UseAuthorization();
 
