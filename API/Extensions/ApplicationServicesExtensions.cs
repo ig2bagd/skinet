516f4912ad2166d3e6cc19f61dc965a85e55d899
steviep <ig2bagd@yahoo.com>
2021-10-03 20:45:17 -0700

    End of section 15

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using API.Errors;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,13 +12,14 @@ namespace API.Extensions
   {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+      services.AddScoped<ITokenService, TokenService>();
       services.AddScoped<IProductRepository, ProductRepository>();
       services.AddScoped<IBasketRepository, BasketRepository>();
       services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
       services.Configure<ApiBehaviorOptions>(options =>
       {
         options.InvalidModelStateResponseFactory = actionContext =>
-              {
+          {
             var errors = actionContext.ModelState
                       .Where(e => e.Value.Errors.Count > 0)
                       .SelectMany(x => x.Value.Errors)
@@ -27,7 +29,7 @@ namespace API.Extensions
             {
               Errors = errors
             };
-
+            
             return new BadRequestObjectResult(errorResponse);
           };
       });
